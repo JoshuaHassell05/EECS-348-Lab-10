a3b31bfcccd32a9ebce7b8361c33632f82485cee
JoshuaHassell05 <157650123+JoshuaHassell05@users.noreply.github.com>
2025-11-05 23:12:38 +0000

    Added loop to read two string per line and defined place holders for parsing

--- calculator.cpp
+++ calculator.cpp
@@ -4,8 +4,13 @@ int main(){
    char filename[256];
    std::cout << "Enter filename: ";
    std::cin >> filename;
-   std ::ifstream fin(filename);
-   
-
+   std ::ifstream in(filename);
+   char A[1024], B[1000];
+   int line = 0;
+   while (in >> A >> B){
+      ++line;
+      bool Sign_A = false, Sign_B = false;
+      char int_A[512], fract_A[512], int_B[512], fract[512];
+   }
    return 0;
 }
